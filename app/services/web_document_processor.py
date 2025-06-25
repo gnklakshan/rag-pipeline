0f84daeea0a06a109bfdee1cef6cd6d76eb4b67c
gnklakshan <132909147+gnklakshan@users.noreply.github.com>
2025-06-25 15:27:31 +0530

    .loadWebAndSplit class

---
+++ app/services/web_document_processor.py
@@ -0,0 +1,20 @@
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+class loadWebAndSplit:
+    def loadWebContents(self,weblinks):
+        web_loader = WebBaseLoader(
+            web_paths=[weblinks],
+            bs_kwargs = dict(parse_only=bs4.SoupStrainer(class_=("post-content", "post-title", "post-header")))
+        )
+
+        docs = web_loader.load()
+        return docs
+
+    def splitWebContent(self,chunckSize,chunckOverlap,docs):
+        splitter = RecursiveCharacterTextSplitter(chunk_size=chunckSize,chunk_overlap=chunckOverlap)
+        return splitter.split_documents(docs)
+
+
+
+
